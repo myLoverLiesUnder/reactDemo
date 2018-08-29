00f7b42389b402410b2ccbb7ed49b21283c603d5
gzx1 <gezx@leadingsoft.cn>
2018-08-29 22:37:08 +0800

    commit

--- src/component/alert.js
+++ src/component/alert.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {Alert } from 'antd';
-import '../css/Alert.css'
+import {Alert} from 'antd';
+import alertCss from '../css/Alert.css'
 
 
 const alert = (props) => {
     return (
-        <div className="alertBody">
-            <Alert message={props.message} type={props.type} showIcon　closable onClose={props.onClose} />
+        <div className={alertCss.alertBody}>
+            <Alert message={props.message} type={props.type} showIcon closable onClose={props.onClose}/>
         </div>
     )
 };
