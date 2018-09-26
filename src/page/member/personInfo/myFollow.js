e0c68c1997ad6fabaab867ac252866e5e767ec7a
gzx1 <gezx@leadingsoft.cn>
2018-09-26 21:54:13 +0800

    commit

--- src/page/member/personInfo/myFollow.js
+++ src/page/member/personInfo/myFollow.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {getFollowsList} from "../../../axios/api"
-import {Row, Col, Card, Icon} from 'antd'
+import {Row, Col} from 'antd'
 import listCss from '../../../css/custom.css'
 
 
@@ -18,20 +18,44 @@ export default class myFollow extends React.Component {
         })
     }
 
+    hoverEnter = (index) => {
+        this.refs[index].style.display = 'block'
+    };
+
+    hoverLeave = (index) => {
+        this.refs[index].style.display = 'none'
+    };
+
+    cancelFollow = (index) => {
+        let followList = this.state.follow;
+        followList.splice(index,1);
+        this.setState({
+            follow: followList
+        })
+    };
+
     render() {
         return (
             <div>
                 <Row gutter={16}>
-                    {this.state.follow.map((item) =>
+                    {this.state.follow.map((item, index) =>
                         <Col key={item.roomid} span={12}>
-                            <div className={listCss.box}>
+                            <div className={listCss.box}
+                                 onMouseEnter={() => this.hoverEnter(index)}
+                                 onMouseLeave={() => this.hoverLeave(index)}>
                                 <div className={listCss.fPic}>
                                     <img src={item.pic} alt="example"/>
                                 </div>
                                 <div className={listCss.fInfo}>
                                     <h1>{item.roomname}</h1>
                                     <p>{item.nickname}</p>
-                                    <p style={{color:'#ff630e'}}>{item.gamename}</p>
+                                    <p style={{color: '#ff630e'}}>{item.gamename}</p>
+                                    <p className={listCss.cancelFocus}>
+                                        <a ref={index} href="" onClick={(e) => {
+                                            e.preventDefault();
+                                            this.cancelFollow(index)
+                                        }}>取消关注</a>
+                                    </p>
                                 </div>
                             </div>
                         </Col>)
